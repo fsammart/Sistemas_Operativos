2ac378658fcfa0f87ccd04e9dec9d25477aab7a5
jpalacci <jpalacci@itba.edu.ar>
2017-09-05 18:19:03 -0300

    Changes have been made

--- pipes8.c
+++ pipes8.c
@@ -7,27 +7,33 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #define SONS 3
+#define READ_END 0
+#define WRITE_END 1
+#define BUFF_MAX 152
+#define AUX_MAX 1000
+#define COMMAND_MAX 100
+#define CHILD_NUMBER buff[0]-'0'
 
 void startListening(int fdread, int fdwrite, int son)
 {
-	char buff[152];
-	char num[154];
-	char command[100];
+	char buff[BUFF_MAX];
+	char num[BUFF_MAX];
+	char command[COMMAND_MAX];
 	int readbytes;
-	char aux[1000];
+	char aux[AUX_MAX];
 
 	strcpy(command, "md5sum");
 	command[6] = ' ';
 	command[7] = 0;
 
-	while((readbytes = read(fdread,buff,152)) > 0)
+	while((readbytes = read(fdread,buff,BUFF_MAX)) > 0)
 	{	
 		command[7] = 0;
 		sprintf(num, "%d", son);	
 		printf("I am your son number %d and I received the file: %s\n", son, buff);
 		strcat(command, buff);
 		FILE * file = popen(command, "r");	
-		fgets(aux, 1000, file);	
+		fgets(aux, AUX_MAX, file);	
 		strcat(num, aux);
 		write(fdwrite, num, strlen(num) + 1);					
 	}
@@ -72,22 +78,11 @@ void initializePipes(int pipearr[][2], int q)
 
 }
 
-void closeUnnecessaryEndsChild(int pipearr[2], int tofather[2])
-{
-	close(pipearr[1]);
-	close(tofather[0]);
-}
-
-void closeUnnecesaryEndsFather(int tofather[2], int son[2])
-{
-	close(tofather[1]);
-	close(son[0]);
-}
 
 void allocatingNewFile(int pipearr[2], char * file, int length, int son)
 {
 	printf("Allocating new file %s to son number %d \n", file, son);
-	write(pipearr[1], file, length);
+	write(pipearr[WRITE_END], file, length);
 }
 
 int main(int argc, char * argv[])
@@ -95,23 +90,25 @@ int main(int argc, char * argv[])
 	pid_t pids[SONS];
 	int pid;
 	int pipearr[SONS + 1][2];
-	char buff[152];
+	char buff[BUFF_MAX];
 	int workDoneByEach[SONS] = {1, 1, 1};
-	double minForEach = (double)(argc - 1) / SONS;
-	int overload = argc - 1 - (int)minForEach * SONS;
+	int minForEach = (argc - 1) / SONS;
+	int overload = argc - 1 - minForEach * SONS;
 	char * shm, * s;
 
 
 	initializePipes(pipearr, SONS + 1);
-
+	
 	for(int k = 0; k < SONS; k++)
 	{
 
 		pid = fork();
 		if(pid == 0)
 		{
-			closeUnnecessaryEndsChild(pipearr[k], pipearr[SONS]);
-			startListening(pipearr[k][0], pipearr[SONS][1], k);
+			close(pipearr[SONS][READ_END]);
+			close(pipearr[k][WRITE_END]);	
+			startListening(pipearr[k][READ_END], pipearr[SONS][WRITE_END], k);	
+			
 			
 		}
 		else
@@ -121,42 +118,42 @@ int main(int argc, char * argv[])
 				perror("fork");
 				exit (1);
 			}
-
-			closeUnnecesaryEndsFather(pipearr[SONS], pipearr[k]);
+			
+			close(pipearr[k][READ_END]);
 			pids[k] = pid;
 		
 		}
 		
 	}
 
-
+	close(pipearr[SONS][WRITE_END]);
 	shm = createSharedMemory(getpid());
 	shm[0] = 1;
 	s = shm + 1;
-
-	for(int m = 1; m < 4 && m < argc; m++)
+	int jobNumber;
+	for(jobNumber = 1; jobNumber < SONS + 1 && jobNumber < argc; jobNumber++)
 	{	
-		write(pipearr[m - 1][1], argv[m], strlen(argv[m]) + 1);
+		write(pipearr[jobNumber - 1][WRITE_END], argv[jobNumber], strlen(argv[jobNumber]) + 1);
 		
 	}
 
-	for(;m < argc; m++)
+	for(;jobNumber < argc; jobNumber++)
 	{
-		read(pipearr[SONS][0], buff, 100);
+		read(pipearr[SONS][READ_END], buff, BUFF_MAX);
 		strcat(s, buff);
 
-		if(workDoneByEach[buff[0] - '0'] < (int)minForEach)
+		if(workDoneByEach[CHILD_NUMBER] < minForEach)
 		{
-			allocatingNewFile(pipearr[buff[0] - '0'], argv[m], strlen(argv[m]) + 1, buff[0] - '0');
-			workDoneByEach[buff[0] - '0']++;
+			allocatingNewFile(pipearr[CHILD_NUMBER], argv[jobNumber], strlen(argv[jobNumber]) + 1,CHILD_NUMBER);
+			workDoneByEach[CHILD_NUMBER]++;
 
 		}
 		else
 		{
-			if((overload > 0 )&& (workDoneByEach[buff[0] - '0'] == (int)minForEach))
+			if((overload > 0 )&& (workDoneByEach[CHILD_NUMBER] == minForEach))
 			{
-				allocatingNewFile(pipearr[buff[0] - '0'], argv[m], strlen(argv[m]) + 1, buff[0] - '0');
-				workDoneByEach[buff[0] - '0']++;
+				allocatingNewFile(pipearr[CHILD_NUMBER], argv[jobNumber], strlen(argv[jobNumber]) + 1, CHILD_NUMBER);
+				workDoneByEach[CHILD_NUMBER]++;
 				overload--;
 			}
 		}
@@ -164,7 +161,7 @@ int main(int argc, char * argv[])
 
 	for(int h = 1; h < argc && h < SONS + 1; h++)
 	{	
-		read(pipearr[SONS][0], buff, 100);
+		read(pipearr[SONS][READ_END], buff, BUFF_MAX);
 		strcat(s, buff);
 		//printf("Hash: %s\n", buff+1);
 		
